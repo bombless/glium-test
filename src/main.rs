1871e9b79aefbf51bf38552c2097a4ee09022f7e
York Xiang <bombless@126.com>
2017-05-19 23:33:47 +0800

    Basic demo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
 
 #[macro_use(implement_vertex)]
 extern crate glium;
+extern crate svg;
+
+enum DrawEvent {
+    Line(Vec<Vertex>)
+}
+
+impl DrawEvent {
+    fn get_shape(self) -> Vec<Vertex> {
+        let DrawEvent::Line(line) = self;
+        line
+    }
+}
+
+impl From<svg::SvgEvent> for DrawEvent {
+    fn from(s: svg::SvgEvent) -> Self {
+        let svg::SvgEvent::Line {
+            x1, x2, y1, y2, view_box
+        } = s;
+        let width = view_box[2] - view_box[0];
+        let height = view_box[3] - view_box[1];
+        let x1_ = x1 / width * 2.0 - 1.0;
+        let x2_ = x2 / width * 2.0 - 1.0;
+        let y1_ = y1 / height * 2.0 - 1.0;
+        let y2_ = y2 / height * 2.0 - 1.0;
+
+        let x1_blur = x1_ + 0.003;
+        let x2_blur = x2_ + 0.003;
+        let y1_blur = y1_ + 0.003;
+        let y2_blur = y2_ + 0.003;
+
+        DrawEvent::Line(vec![
+             Vertex { position: [x1_ as f32, y1_ as f32] }, Vertex { position: [x1_blur as f32, y1_blur as f32] }, Vertex { position: [x2_ as f32, y2_ as f32] },
+             Vertex { position: [x2_blur as f32, y2_blur as f32] }, Vertex { position: [x2_ as f32, y2_ as f32] }, Vertex { position: [x1_ as f32, y1_ as f32] },
+        ])
+    }
+}
 
 fn main() {
     use glium::{DisplayBuild,Surface};
@@ -16,12 +52,14 @@ fn main() {
     let mut target = display.draw();
     target.clear_color(0.0, 0.0, 1.0, 1.0);
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
+    let line = svg::parse(r#"
+    <svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+		<line x1="15" y1="0" x2="15" y2="30" stroke-width="2" stroke="black"/>
+	</svg>
+    "#).unwrap();
+    
 
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let vertex_buffer = glium::VertexBuffer::new(&display, &line.into_iter().map(DrawEvent::from).map(DrawEvent::get_shape).fold(Vec::new(), |mut acc, x| { acc.extend(x); acc})).unwrap();
 
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
     
